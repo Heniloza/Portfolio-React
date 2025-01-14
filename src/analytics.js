ecf0ebce66ed97708b2c714f1c57153707956e3f
Heniloza <ozahenil73@gmail.com>
2025-01-14 22:52:33 +0530

    Solved Error

--- src/analytics.js
+++ src/analytics.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga4";
 
 export const initGA = () => {
-  ReactGA.initialize(import.meta.env.VITE_MEASUREMENT_ID); 
+  ReactGA.initialize(import.meta.env.VITE_MEASUREMENT_ID_GOOGLE); 
 };
 
 export const logPageView = () => {
