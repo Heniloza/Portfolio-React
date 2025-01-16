0cff0f66e4e17d00fb240d39945f78c8f25c8c1a
Heniloza <ozahenil73@gmail.com>
2025-01-16 10:31:25 +0530

    Additional information of user added in analytics.js

--- src/analytics.js
+++ src/analytics.js
@@ -1,9 +1,21 @@
 import ReactGA from "react-ga4";
 
 export const initGA = () => {
-  ReactGA.initialize(import.meta.env.VITE_MEASUREMENT_ID); 
+  ReactGA.initialize(import.meta.env.VITE_MEASUREMENT_ID);
 };
 
 export const logPageView = () => {
-  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  const device = navigator.platform;
+  const browser = navigator.userAgent;
+  const screenResolution = `${window.screen.width}x${window.screen.height}`; 
+
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+    title: document.title,
+    location: window.location.href,
+    device, 
+    browser,
+    screenResolution, 
+  });  
 };
