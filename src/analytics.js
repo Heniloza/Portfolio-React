42d94aff25436a9ab3a18efc23ba3a98a8fac8ee
Heniloza <ozahenil73@gmail.com>
2025-01-16 10:41:14 +0530

    Changes done

--- src/analytics.js
+++ src/analytics.js
@@ -7,15 +7,17 @@ export const initGA = () => {
 export const logPageView = () => {
   const device = navigator.platform;
   const browser = navigator.userAgent;
-  const screenResolution = `${window.screen.width}x${window.screen.height}`; 
+  const screenResolution = `${window.screen.width}x${window.screen.height}`;
 
   ReactGA.send({
     hitType: "pageview",
     page: window.location.pathname,
     title: document.title,
     location: window.location.href,
-    device, 
-    browser,
-    screenResolution, 
-  });  
+    customData: {
+      device: navigator.platform, // Custom dimension
+      browser: navigator.userAgent, // Custom dimension
+      screenResolution: `${window.screen.width}x${window.screen.height}`, // Custom dimension
+    },
+  });
 };
